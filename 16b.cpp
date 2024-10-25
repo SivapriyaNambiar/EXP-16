3cfc0214bf1459e7adc184d07073691e6d53f500
SivapriyaNambiar <9sivapriya@gmail.com>
2024-10-25 08:59:32 +0530

    Add files via upload

---
+++ 16b.cpp
@@ -0,0 +1,26 @@
+// NAME - SIVAPRIYA NAMBIAR 
+//PRN - 23070123125
+// EXPERIMENT - 16(B) 
+
+// EXCEPTION HANDLING 
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int age;
+    cout<<"Enter age: ";
+    cin>>age;
+    try{
+        if(age<18) {
+            throw age;
+        }
+        else {
+            cout<<"Age: "<<age<<"\n APPROVED"<<endl;
+        }
+    }
+    catch(int a) {
+        cout<<"\n ERROR: Underage! ("<<age<<")"<<endl;
+    }
+}
